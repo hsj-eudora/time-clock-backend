8dfdd0a0f0a195d873c4e4d7f93771aa16099993
hsyclaire <hsyclaire@gmail.com>
2024-07-23 09:39:09 +0800

    sql update

--- app/Dockerfile/init.sql
+++ app/Dockerfile/init.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: timeclock
+-- Host:     Database: timeclock
 -- ------------------------------------------------------
--- Server version	8.0.33
+-- Server version	8.3.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -20,30 +20,133 @@
 --
 
 CREATE TABLE IF NOT EXISTS `member` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12002 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `UK_mbmcqelty0fbrvxp1q58dn57t` (`email`),
+  UNIQUE KEY `UK_gc3jmn7c2abyo3wf6syln5t2i` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT IGNORE INTO `member` VALUES (12001,'claire','82958654','claire@gmail.com','2024-07-14 15:08:32');
-
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'clair8e@gmail.com','82958654','claire8809999'),(2,'clair8de@gmail.com','82958654','claired88');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-14 23:42:47
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE IF NOT EXISTS `project` (
+  `project_id` bigint NOT NULL AUTO_INCREMENT,
+  `type_id` bigint NOT NULL,
+  `project_name` varchar(255) NOT NULL,
+  `project_desc` text,
+  PRIMARY KEY (`project_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `project_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `project_type` (`type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_type`
+--
+
+CREATE TABLE IF NOT EXISTS `project_type` (
+  `type_id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `type_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `project_type_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_type`
+--
+
+LOCK TABLES `project_type` WRITE;
+/*!40000 ALTER TABLE `project_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `punch`
+--
+
+CREATE TABLE IF NOT EXISTS `punch` (
+  `punch_id` bigint NOT NULL AUTO_INCREMENT,
+  `task_id` bigint NOT NULL,
+  `start_at` timestamp NOT NULL,
+  `end_at` timestamp NULL DEFAULT NULL,
+  `time_count` bigint DEFAULT NULL,
+  PRIMARY KEY (`punch_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `punch_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `punch`
+--
+
+LOCK TABLES `punch` WRITE;
+/*!40000 ALTER TABLE `punch` DISABLE KEYS */;
+/*!40000 ALTER TABLE `punch` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+CREATE TABLE IF NOT EXISTS `tag` (
+  `tag_id` bigint NOT NULL AUTO_INCREMENT,
+  `project_id` bigint NOT NULL,
+  `tag_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`tag_id`),
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE IF NOT EXISTS `task` (
+  `task_id` bigint NOT NULL AUTO_INCREMENT,
+  `project_id` bigint NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `task_desc` text,
+  `est_time` bigint DEFAULT NULL,
+  `est_comp` bigint DEFAULT NULL,
+  `est_ave_comp` bigint DEFAULT NULL,
+  `act_total_time` bigint DEFAULT NULL,
+  `act_ave_comp` bigint DEFAULT NULL,
+  PRIMARY KEY (`tas
